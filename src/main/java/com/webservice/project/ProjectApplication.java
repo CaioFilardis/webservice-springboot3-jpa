67a4b10a0b0c415a98ffef00b21922cd9bee4594
Caio Filardis <caiosantosshake@hotmail.com>
2025-07-25 21:59:07 -0300

    Project created

---
+++ src/main/java/com/webservice/project/ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.webservice.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // configura o projeto para dizer que é úm projeto Spring Boot
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
